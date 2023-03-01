23b3a1c907b40cafda839f314f0ae5a7ec311757
Daniil Davydovskiy <danildad.dad@yandex.ru>
2023-03-01 17:39:36 +0300

    feat: добавить временное решение авторизации

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/mestodb");
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "63fc9f70965889b8e5e3f086",
+  };
+
+  next();
+});
+
+module.exports.createCard = (req, res) => {
+  console.log(req.user._id); // _id станет доступен
+};
+
 app.use("/users", require("./routes/users"));
 app.use("/cards", require("./routes/cards"));
 
