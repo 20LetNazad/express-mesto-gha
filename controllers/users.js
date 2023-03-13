c2c0c7d00f8f6a6d3f8e1b4961be3bc981ad6a86
Daniil Davydovskiy <danildad.dad@yandex.ru>
2023-03-13 19:52:23 +0300

    feat: валидация пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -1,47 +1,97 @@
 const User = require("../models/user");
+const NotFoundError = require("../errors/NotFoundError");
+const BadRequestError = require("../errors/BadRequestError");
 
 module.exports.findUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch((err) =>
-      res.status(500).send({ message: `Something went wrong ${err}` })
-    );
+    .catch((err) => res.status(500).send({ message: err }));
 };
 
-module.exports.findUserById = (req, res) => {
+module.exports.findUserById = (req, res, next) => {
   User.findById(req.params.userId)
+    .then((user) => {
+      if (!user) {
+        next(new NotFoundError("User not found"));
+      } else {
+        res.send({ data: user });
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+module.exports.createUser = (req, res, next) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({
+    name,
+    about,
+    avatar,
+  })
     .then((user) => {
       res.send({ data: user });
     })
-    .catch((err) =>
-      res.status(500).send({ message: `Something went wrong ${err}` })
-    );
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        next(new BadRequestError("Incorrect data was transmitted"));
+      } else {
+        next(err);
+      }
+    });
 };
 
-module.exports.createUser = (req, res) => {
-  User.create({ ...req.body })
-    .then((user) => res.send({ data: user }))
-    .catch((err) =>
-      res.status(500).send({ message: `Something went wrong ${err}` })
-    );
-};
-
-module.exports.editUser = (req, res) => {
+module.exports.editUser = (req, res, next) => {
   const { name, about } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { name, about })
-    .then((user) => res.send({ data: user }))
-    .catch((err) =>
-      res.status(500).send({ message: `Something went wrong ${err}` })
-    );
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .then((user) => {
+      if (user === null) {
+        next(new NotFoundError("User not found"));
+      } else {
+        res.send({ data: user });
+      }
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        next(new BadRequestError("Incorrect data was transmitted"));
+      } else {
+        next(err);
+      }
+    });
 };
 
-module.exports.editAvatar = (req, res) => {
+module.exports.editAvatar = (req, res, next) => {
   const { avatar } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) =>
-      res.status(500).send({ message: `Something went wrong ${err}` })
-    );
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .then((user) => {
+      if (!user) {
+        next(new NotFoundError("User not found"));
+      } else {
+        res.send({ data: user });
+      }
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        next(new BadRequestError("Incorrect data was transmitted"));
+      } else {
+        next(err);
+      }
+    });
 };
