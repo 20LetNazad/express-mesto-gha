eaf2308087576c8810d468484780b0410c704de8
Daniil Davydovskiy <danildad.dad@yandex.ru>
2023-03-30 13:35:33 +0300

    feat: add user auth

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 module.exports.findUsers = (req, res) => {
@@ -27,13 +28,18 @@ module.exports.findUserById = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-
-  User.create({
-    name,
-    about,
-    avatar,
-  })
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
     .then((user) => {
       res.status(200).send({ data: user });
     })
